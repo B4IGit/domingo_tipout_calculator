20efad970b2541d2e14ea90fe79d5d11ae5be72d
Devin Ledesma <dev.dfledesma@gmail.com>
2026-08-25 10:57:04 -0500

    feat: add reusable ThemeCard component

---
+++ components/ThemedCard.jsx
@@ -0,0 +1,21 @@
+import {View, useColorScheme, StyleSheet} from 'react-native';
+import {Colors} from '../constants/Colors';
+
+const ThemedCard = ({style, children, ...props}) => {
+    const colorScheme = useColorScheme();
+    const theme = Colors[colorScheme] ?? Colors.light;
+
+    return (
+        <View style={[{backgroundColor: theme.uiBackground}, styles.card, style]} {...props}>
+            {children}
+        </View>
+    );
+};
+export default ThemedCard;
+
+const styles = StyleSheet.create({
+    card: {
+        borderRadius: 5,
+        padding: 20
+    }
+});
